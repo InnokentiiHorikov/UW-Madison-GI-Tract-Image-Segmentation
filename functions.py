47e4a6097f398c9fb947f1710b9861413af3051e
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-01-10 19:00:12 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,22 +9,22 @@ def empty_cached():
 
 
 def get_path(data, path):
-  train['path'] = path
-  train['splitted'] = train.id.agg(lambda x: x.split('_'))
-  train['case']     = train.splitted.agg(lambda x: str(x[0]))
-  train['day']      = train.splitted.agg(lambda x: str(x[1]))
-  train['slice']    = train.splitted.agg(lambda x: int(x[3].lstrip('0')))
+  data['path'] = path
+  data['splitted'] = data.id.agg(lambda x: x.split('_'))
+  data['case']     = data.splitted.agg(lambda x: str(x[0]))
+  data['day']      = data.splitted.agg(lambda x: str(x[1]))
+  data['slice']    = data.splitted.agg(lambda x: int(x[3].lstrip('0')))
 
 
-  cases = train.case.value_counts(sort = False)
+  cases = data.case.value_counts(sort = False)
   cases_val, case_iter = cases.index, cases.values
   case_iter = np.insert(case_iter, 0, 0)
   case_iter = np.cumsum(case_iter)
 
   for i in range(1, case_iter.shape[0]):
     
-      temp_data = train.iloc[case_iter[i-1]:case_iter[i]]
-      train.path.iloc[case_iter[i-1]:case_iter[i]] +=  cases_val[i-1]
+      temp_data = data.iloc[case_iter[i-1]:case_iter[i]]
+      data.path.iloc[case_iter[i-1]:case_iter[i]] +=  cases_val[i-1]
 
       days = temp_data.day.value_counts(sort = False)
       day_val, day_iter = days.index, days.values
@@ -34,16 +34,16 @@ def get_path(data, path):
       for j in range(1, day_iter.shape[0]):
         
           val = '/'+cases_val[i-1]+'_'+day_val[j-1]+'/'+'scans/'
-          train.path.iloc[case_iter[i-1]+day_iter[j-1]:day_iter[j]+case_iter[i-1]] +=  val
+          data.path.iloc[case_iter[i-1]+day_iter[j-1]:day_iter[j]+case_iter[i-1]] +=  val
 
-          list_of_files = sorted(os.listdir(train.path.iloc[case_iter[i-1]+day_iter[j-1]]))
+          list_of_files = sorted(os.listdir(data.path.iloc[case_iter[i-1]+day_iter[j-1]]))
 
-          train.path.iloc[case_iter[i-1]+day_iter[j-1]:day_iter[j]+case_iter[i-1]] +=  list_of_files
+          data.path.iloc[case_iter[i-1]+day_iter[j-1]:day_iter[j]+case_iter[i-1]] +=  list_of_files
 
   return data
 
 
-def preprocessing(data):
+def preprocessing(data, path):
   
   data['missed'] =  train.segmentation.notna()
   data['segm'] = ''
@@ -51,11 +51,13 @@ def preprocessing(data):
   for i in range(0, train.shape[0], 3):
       data.segm.iloc[i] = data.iloc[i:i+3].segmentation.to_numpy()
       data.missed.iloc[i] = data.iloc[i:i+3].missed.any()
-  print(train.info())  
+  print(data.info())  
 
-  train = train[::3]
-  train.reset_index(drop = True, inplace = True)
+  data = train[::3]
+  data.reset_index(drop = True, inplace = True)
 
+  data = get_path(data, path)
+  
 
 def RLE_masking(rle, shape = (360, 360)):
     
