dee9c15a7d953a496649bd28ad5634d209e1a0a2
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-01-10 18:44:09 +0300

    Create train_test_model.py

---
+++ train_test_model.py
@@ -0,0 +1,54 @@
+from Dataset import *
+import tqdm
+
+def train_test_model(data, model,
+                     loss_fn,
+                     optimizer, metrics, epochs):
+  dataset = ImageDataset(train)
+  trainset, validset = random_split(dataset, [0.9, 0.1])
+  train_dataloader = DataLoader(trainset, shuffle=True, batch_size=batch_size)
+  valid_dataloader = DataLoader(validset, shuffle=True, batch_size=batch_size)
+  
+  for i in range(epochs):
+    print(f"Epoch {i}")
+    
+    train_tqdm = tqdm.tqdm(train_dataloader)
+    model.train()
+    
+    for batch in train_tqdm:
+        image, masks = batch['image'], batch['masks']
+        masks = masks[:, :, :352, :352]
+        image, masks = image.to(device), masks.to(device) 
+    
+        optimizer.zero_grad()  
+        model.zero_grad()
+        
+        output = model(image)
+        
+        L = loss_fn(output, masks)
+        M = metrics(output.to(torch.short), masks.to(torch.short))
+ 
+
+        L.backward()   
+
+        optimizer.step()  
+        train_tqdm.set_description(f"Train loss: {L.item()}  Metrics loss: {M[0]}-{M[1]}-{M[2]}")
+        train_tqdm.refresh() 
+
+
+    model.eval()
+    valid_metrics = torch.tensor([0, 0, 0], dtype = torch.float)
+    test_tqdm = tqdm.tqdm(valid_dataloader)
+    with torch.no_grad():
+        for batch in test_tqdm:
+            image, masks = batch['image'], batch['masks']
+            masks = masks[:, :, :352, :352]
+            image, masks = image.to(device), masks.to(device) 
+            
+            output = model(image)
+            
+            M = metrics(output.to(torch.short), masks.to(torch.short))
+            valid_metrics = torch.vstack((valid_metrics, M.cpu()))
+
+    print(f"Epoch {i}, Metrics: {torch.mean(valid_metrics[1:, :], axis = 0)}")
+                            
