6c07e95107692a0e390cad5e6d05f8e8fd7c7205
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-01-10 19:02:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import preproc
 import torch
 from torch import nn 
+from train_test_model import *
 from torchmetrics.segmentation import GeneralizedDiceScore
 
 
@@ -20,10 +21,14 @@ batch_size = 12
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3,weight_decay=1e-2) 
 loss_fn = nn.BCEWithLogitsLoss() 
 metrics = GeneralizedDiceScore(num_classes=3, per_class=True).to(device)
+model = UNet(n_channels = 1, n_classes = 3).to(dtype=torch.half, device='cuda')
 
 if __name__ == "__main__":
   data = pd.read_csv(path)
-  data = preproc.preprocessing(data)
+  data = preproc.preprocessing(data, path)
 
-  
+  train_test_model(data, model, 
+                   loss_fn,optimizer, metrics,
+                   10)
+    
   
