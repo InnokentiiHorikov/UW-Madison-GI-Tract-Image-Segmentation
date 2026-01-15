81f27dff886a8420b237536394f66cfbd4e0a5e2
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-01-15 18:23:51 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import torch
 import pandas as pd
 import gc
-
+import cv2
 
 def empty_cached():
   gc.collect()
@@ -78,3 +78,48 @@ def RLE_masking(rle, shape = (360, 360)):
     mask = torch.reshape(mask, (shape[0], shape[1]))
     
     return mask
+
+
+def find_contour(img):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    thresh = cv2.threshold(gray, 15, 255, cv2.THRESH_BINARY)[1]
+    #thresh = cv2.erode(thresh, None, iterations=2)
+    #thresh = cv2.dilate(thresh, None, iterations=2)
+
+    cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    c = max(cnts, key=cv2.contourArea)
+
+    extreme_point = []
+    extLeft = tuple(c[c[:,:,0].argmin()][0]); extreme_point.append(extLeft)
+    extRight = tuple(c[c[:,:,0].argmax()][0]); extreme_point.append(extRight)
+    extTop = tuple(c[c[:,:,1].argmin()][0]); extreme_point.append(extTop) 
+    extBot = tuple(c[c[:,:,1].argmax()][0]); extreme_point.append(extBot)
+    
+    img_cnt = cv2.drawContours(img.copy(), c, -1, (0,255,255), 3)
+
+    img_pnt = cv2.circle(img_cnt.copy(), extLeft, 5, (0, 0, 255), -1)
+    img_pnt = cv2.circle(img_pnt, extRight, 5, (0, 255, 0), -1)
+    img_pnt = cv2.circle(img_pnt, extTop, 5, (255, 0, 0), -1)
+    img_pnt = cv2.circle(img_pnt, extBot, 5, (255, 255, 0), -1)
+    
+    return img_pnt, extreme_point
+
+
+def crop_img(img, extreme_point):
+    
+    new_img = img[extreme_point[2][1]:extreme_point[3][1], extreme_point[0][0]:extreme_point[1][0]]
+    
+    return new_img
+
+
+def remove_noise(img, extreme_point):
+
+    new_img = np.zeros((img.shape[0],img.shape[1], img.shape[2]), dtype=np.uint8)
+    
+    new_img[extreme_point[2][1]:extreme_point[3][1], extreme_point[0][0]:extreme_point[1][0]] = img[extreme_point[2][1]:extreme_point[3][1], extreme_point[0][0]:extreme_point[1][0]]
+    
+    return new_img
+
+
