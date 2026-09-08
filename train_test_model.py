da1d0f4018d1e3b736a8c8db3a0a9a4c3ce875e3
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-09-08 22:40:40 +0300

    Update train_test_model.py

--- train_test_model.py
+++ train_test_model.py
@@ -9,7 +9,7 @@ def train_test_model(data, model,
   train_dataloader = DataLoader(trainset, shuffle=True, batch_size=batch_size)
   valid_dataloader = DataLoader(validset, shuffle=True, batch_size=batch_size)
   
-  for i in range(epochs):
+ for i in range(epochs):
     print(f"Epoch {i}")
     
     train_tqdm = tqdm.tqdm(train_dataloader)
@@ -17,46 +17,42 @@ def train_test_model(data, model,
     
     for batch in train_tqdm:
         image, masks = batch['image'], batch['masks']
-        masks = masks[:, :, :352, :352]
         image, masks = image.to(device), masks.to(device) 
-    
         optimizer.zero_grad() 
         model.zero_grad()
         
         output = model(image)
+
         L = loss_fn(output, masks)
-        output, masks = output.cpu(), masks.cpu()
+        L.backward()
+
+        output = torch.round(F.sigmoid(output))
+        M = metrics(output.to(torch.int64), masks.to(torch.int64))
         
-        M = metrics(3, output, masks)
- 
-
-        L.backward() 
-        nn.utils.clip_grad_value_(model.parameters(), clip_value=0.1)
-
         optimizer.step()  
-        train_tqdm.set_description(f"Train loss: {L.item()}  Metrics loss: {M[0]}-{M[1]}-{M[2]}")
+        train_tqdm.set_description(f"Train loss: {L.item()}  Metrics loss: {M}") 
         train_tqdm.refresh() 
 
-
     model.eval()
-    valid_metrics = torch.tensor([0, 0, 0], dtype = torch.float)
+    valid_metrics = torch.tensor([0, 0, 0], dtype = torch.float).to(device)
     val_loss = []
     
     test_tqdm = tqdm.tqdm(valid_dataloader)
+
     with torch.no_grad():
         for batch in test_tqdm:
             image, masks = batch['image'], batch['masks']
-            masks = masks[:, :, :352, :352]
+
             image, masks = image.to(device), masks.to(device) 
             
             output = model(image)
+
             L = loss_fn(output, masks)
             val_loss.append(L.item())
             
-            output, masks = output.cpu(), masks.cpu()
-            M = metrics(3, output, masks)
+            output = torch.round(F.sigmoid(output))
+            M = metrics(output.to(torch.int64), masks.to(torch.int64))
             
             valid_metrics = torch.vstack((valid_metrics, M))
-
+            
     print(f"Epoch {i}, Loss: {np.mean(val_loss)} Metrics: {torch.mean(valid_metrics[1:, :], axis = 0)}")
-                            
