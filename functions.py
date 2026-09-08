4fd0d8e50a6b1081ed33ca90561b79b0130ea8ba
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-09-08 22:31:16 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -59,67 +59,33 @@ def preprocessing(data, path):
   data = get_path(data, path)
   
 
-def RLE_masking(rle, shape = (360, 360)):
+def RLE_masking(rle_arr: np.array(str | None), 
+                shape: (3, int, int, int)) -> torch.Tensor:
+    """
+    Ouput: np.array[3, Depth, Width, Height] of RLE masks
+    for original(not transformed) 3D Object"
+    """
     
-    if pd.isna(rle):
-        return torch.zeros(shape[0], shape[1])
-
-    size_of_image = shape[0]*shape[1]
-
-    rle = list(map(int, rle.split()))
-        
-    start_pixel, len_of_masking = rle[::2], rle[1::2]
-    mask = torch.zeros(size_of_image)
+    RLE = torch.zeros((shape[1], shape[0], shape[2], shape[3]))
     
-    masking = torch.hstack([torch.arange(start_pixel[i], start_pixel[i]+len_of_masking[i] + 1) 
-                        for i in range(len(start_pixel))]) - 1
-
-    mask[masking] = 1
-    mask = torch.reshape(mask, (shape[0], shape[1]))
+    for j in range(shape[1]):
+        for i in range(shape[0]):
+            
+            if pd.isna(rle_arr[j][i]):
+                continue
     
-    return mask
-
-
-def find_contour(img):
-
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    thresh = cv2.threshold(gray, 15, 255, cv2.THRESH_BINARY)[1]
-    #thresh = cv2.erode(thresh, None, iterations=2)
-    #thresh = cv2.dilate(thresh, None, iterations=2)
-
-    cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    c = max(cnts, key=cv2.contourArea)
-
-    extreme_point = []
-    extLeft = tuple(c[c[:,:,0].argmin()][0]); extreme_point.append(extLeft)
-    extRight = tuple(c[c[:,:,0].argmax()][0]); extreme_point.append(extRight)
-    extTop = tuple(c[c[:,:,1].argmin()][0]); extreme_point.append(extTop) 
-    extBot = tuple(c[c[:,:,1].argmax()][0]); extreme_point.append(extBot)
+            rle = list(map(int, rle_arr[j][i].split()))
+            start_pixel, len_of_masking = rle[::2], rle[1::2]
+            temp = torch.zeros((shape[2]*shape[3]))
+            
+            masking = torch.hstack(
+                [torch.arange(start_pixel[i], start_pixel[i]+len_of_masking[i])
+                                for i in range(len(start_pixel))]) 
+            
+            temp[masking] = 1 
+            RLE[j][i] = temp.reshape((shape[2], shape[3]))
     
-    img_cnt = cv2.drawContours(img.copy(), c, -1, (0,255,255), 3)
-
-    img_pnt = cv2.circle(img_cnt.copy(), extLeft, 5, (0, 0, 255), -1)
-    img_pnt = cv2.circle(img_pnt, extRight, 5, (0, 255, 0), -1)
-    img_pnt = cv2.circle(img_pnt, extTop, 5, (255, 0, 0), -1)
-    img_pnt = cv2.circle(img_pnt, extBot, 5, (255, 255, 0), -1)
-    
-    return img_pnt, extreme_point
-
-
-def crop_img(img, extreme_point):
-    
-    new_img = img[extreme_point[2][1]:extreme_point[3][1], extreme_point[0][0]:extreme_point[1][0]]
-    
-    return new_img
-
-
-def remove_noise(img, extreme_point):
-
-    new_img = np.zeros((img.shape[0],img.shape[1], img.shape[2]), dtype=np.uint8)
-    
-    new_img[extreme_point[2][1]:extreme_point[3][1], extreme_point[0][0]:extreme_point[1][0]] = img[extreme_point[2][1]:extreme_point[3][1], extreme_point[0][0]:extreme_point[1][0]]
-    
-    return new_img
+    RLE = RLE.permute(1, 0, 2, 3)
+    return RLE
 
 
