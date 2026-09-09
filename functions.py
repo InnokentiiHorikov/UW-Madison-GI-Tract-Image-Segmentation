513f0d889298c97c91e8b2631dd3f573d968ec4c
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-09-09 17:05:59 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,56 +8,53 @@ def empty_cached():
   torch.cuda.empty_cache()
 
 
-def get_path(data, path):
-  data['path'] = path
-  data['splitted'] = data.id.agg(lambda x: x.split('_'))
-  data['case']     = data.splitted.agg(lambda x: str(x[0]))
-  data['day']      = data.splitted.agg(lambda x: str(x[1]))
-  data['slice']    = data.splitted.agg(lambda x: int(x[3].lstrip('0')))
+def preprocessing_trainset(trainset, path):
+
+  for i in range(0, data.shape[0], 3):
+    trainset['segmentation'].iloc[i] = trainset['segmentation'].iloc[i:i+3].to_numpy()
+
+  trainset = trainset[::3]
+  trainset.reset_index(drop = True, inplace = True)
+  
+  trainset['path']     = path
+  trainset['splitted'] = trainset['id'].agg(lambda x: x.split('_'))
+  trainset['case']     = trainset['splitted'].agg(lambda x: str(x[0]))
+  trainset['day']      = trainset['splitted'].agg(lambda x: str(x[1]))
+  trainset['slice']    = trainset['splitted'].agg(lambda x: int(x[3].lstrip('0')))
 
 
-  cases = data.case.value_counts(sort = False)
+  cases = trainset.case.value_counts(sort = False)
   cases_val, case_iter = cases.index, cases.values
   case_iter = np.insert(case_iter, 0, 0)
   case_iter = np.cumsum(case_iter)
-
+  
   for i in range(1, case_iter.shape[0]):
-    
-      temp_data = data.iloc[case_iter[i-1]:case_iter[i]]
-      data.path.iloc[case_iter[i-1]:case_iter[i]] +=  cases_val[i-1]
-
+      
+      temp_data = trainset.loc[case_iter[i-1]:case_iter[i]-1]
+  
       days = temp_data.day.value_counts(sort = False)
       day_val, day_iter = days.index, days.values
+  
       day_iter = np.insert(day_iter, 0, 0)
       day_iter = np.cumsum(day_iter)
-
+  
       for j in range(1, day_iter.shape[0]):
-        
-          val = '/'+cases_val[i-1]+'_'+day_val[j-1]+'/'+'scans/'
-          data.path.iloc[case_iter[i-1]+day_iter[j-1]:day_iter[j]+case_iter[i-1]] +=  val
-
-          list_of_files = sorted(os.listdir(data.path.iloc[case_iter[i-1]+day_iter[j-1]]))
-
-          data.path.iloc[case_iter[i-1]+day_iter[j-1]:day_iter[j]+case_iter[i-1]] +=  list_of_files
-
-  return data
-
-
-def preprocessing(data, path):
+          
+          first_idx = case_iter[i-1]+day_iter[j-1]
+          second_idx = day_iter[j]+case_iter[i-1]-1
   
-  data['missed'] =  train.segmentation.notna()
-  data['segm'] = ''
-
-  for i in range(0, train.shape[0], 3):
-      data.segm.iloc[i] = data.iloc[i:i+3].segmentation.to_numpy()
-      data.missed.iloc[i] = data.iloc[i:i+3].missed.any()
-  print(data.info())  
-
-  data = train[::3]
-  data.reset_index(drop = True, inplace = True)
-
-  data = get_path(data, path)
+          val = cases_val[i-1]+'/'+cases_val[i-1]+'_'+day_val[j-1]+'/'+'scans/'
+          
+          trainset['path'].loc[first_idx] +=  val
   
+          trainset['segmentation'].loc[first_idx] = data['segmentation'].loc[first_idx:second_idx].tolist()
+  
+          trainset.drop(data.loc[first_idx+1:second_idx].index, 
+                 inplace = True)
+  
+    trainset.reset_index(inplace = True, drop = True)
+    return trainset
+    
 
 def RLE_masking(rle_arr: np.array(str | None), 
                 shape: (3, int, int, int)) -> torch.Tensor:
